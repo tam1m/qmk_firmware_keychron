29919977fb5fdc3f17db1497b97a7b0c1728f117
tam1m <tam1m@users.noreply.github.com>
2024-02-04 16:21:21 +0100

    Keymap update

--- keyboards/keychron/q3_max/ansi_encoder/keymaps/my_via/keymap.c
+++ keyboards/keychron/q3_max/ansi_encoder/keymaps/my_via/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,       KC_BSLS,    KC_DEL,   KC_END,   KC_PGDN,
         MO(1),    KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                 KC_ENT,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,                 KC_RSFT,              KC_UP,
-        KC_LCTL,  KC_LCMD,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RWIN,  LT(1,KC_APP),  KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT),
+        KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RWIN,  LT(1,KC_APP),  KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT),
 
 
     [TWO] = LAYOUT_tkl_ansi(
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_P7,    KC_P8,    KC_P9,    XXXXXXX,  XXXXXXX,    KC_PSLS,    KC_P1,    KC_P2,    KC_P3,
         KC_CAPS,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  KC_P4,    KC_P5,    KC_P6,    XXXXXXX,              KC_PAST,
         _______,            _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  BAT_LVL,  XXXXXXX,  XXXXXXX,  KC_P1,    KC_P2,    KC_P3,                MO(2),                _______,
-        _______,  XXXXXXX,  _______,                                _______,                                KC_P0,    KC_PDOT,  XXXXXXX,    KC_RCTL,    KC_P0,    KC_PDOT,  KC_PENT),
+        _______,  KC_LGUI,  _______,                                _______,                                KC_P0,    KC_PDOT,  XXXXXXX,    KC_RCTL,    KC_P0,    KC_PDOT,  KC_PENT),
 
 
     [THREE] = LAYOUT_tkl_ansi(
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,    _______,  _______,  _______,
         _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,              QK_LOCK,
         _______,            _______,  _______,  _______,  _______,  BAT_LVL,  NK_TOGG,  _______,  _______,  _______,  MY_PW,                _______,              _______,
-        _______,  _______,  _______,                                _______,                                _______,  GU_TOGG,  XXXXXXX,    _______,    _______,  _______,  _______),
+        _______,  GU_TOGG,  _______,                                _______,                                _______,  GU_TOGG,  XXXXXXX,    _______,    _______,  _______,  _______),
 
 
     [FOUR] = LAYOUT_tkl_ansi(
