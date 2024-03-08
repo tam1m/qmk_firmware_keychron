14c0e045d81a5ec6ea54e3d77e03b3b7463e7a66
tam1m <tam1m@users.noreply.github.com>
2024-03-08 14:06:43 +0100

    change default rgb color

--- keyboards/keychron/q3_max/ansi_encoder/keymaps/my_via/config.h
+++ keyboards/keychron/q3_max/ansi_encoder/keymaps/my_via/config.h
@@ -2,7 +2,7 @@
 // #define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_TYPING_HEATMAP
 #define RGB_MATRIX_DEFAULT_SPD 127 // Sets the default animation speed, if none has been set
 #define RGB_MATRIX_DEFAULT_HUE 162 // (230° unint8) Sets the default hue value, if none has been set
-#define RGB_MATRIX_DEFAULT_SAT 200 // (63% unint8)  Sets the default saturation value, if none has been set
+#define RGB_MATRIX_DEFAULT_SAT 255 // (63% unint8)  Sets the default saturation value, if none has been set
 #define RGB_MATRIX_DEFAULT_VAL 127 // (50% unint8)  Sets the default hue value, if none has been set
 #define RGB_MATRIX_DEFAULT_ON true
 // #define FORCE_NKRO
